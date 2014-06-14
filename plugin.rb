eeb5eadeca17835644db293b7582fc913b243b13
NomNuggetNom <nomnuggetnom@gmail.com>
2014-06-14 18:35:25 -0400

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: fontawesome
+# about: a plugin that allows for insertion of fontawesome icons
+# version: 0.1
+# authors: NomNuggetNom
+
+register_asset "javascripts/fontawesome.js", :server_side
