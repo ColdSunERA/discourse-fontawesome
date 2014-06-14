e22e85c6f19b696317240cfd0782b45ed05d3e90
NomNuggetNom <nomnuggetnom@gmail.com>
2014-06-14 18:36:06 -0400

    Create fontawesome.js

---
+++ assets/javascripts/fontawesome.js
@@ -0,0 +1,17 @@
+function fontawesome(text) {
+  //Expression to find.
+  var re = /\[head:([a-zA-Z0-9]+)]/;
+  // Adjust text.
+  var adjusted = text.replace(re, "<img src=\"http://cravatar.eu/helmavatar/" + "$1" + "/16.png\">");
+  return adjusted;
+}
+
+Discourse.Dialect.postProcessText(function (text) {
+  text = [].concat(text);
+  for (var i = 0; i < text.length; i++) {
+    if (text[i].length > 0 && text[i][0] !== "<") {
+      text[i] = fontawesome(text[i]);
+    }
+  }
+  return text;
+});
