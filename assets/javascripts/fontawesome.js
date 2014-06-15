7155574835c1700afdc2f0df7adf9d3dc2fc7c3a
NomNuggetNom <nomnuggetnom@gmail.com>
2014-06-14 20:44:17 -0400

    Comments

--- assets/javascripts/fontawesome.js
+++ assets/javascripts/fontawesome.js
@@ -1,17 +1,23 @@
 //Use: [fa:iconname] ex [fa:circle]
 
 function fontawesome(text) {
+  
   //Expression to find.
   var re = /\[fa:\s*([a-zA-Z0-9-]+)]/;
+  
   // Adjust text.
   var adjusted = text.replace(re, "<i class=\"fa fa-" + "$1" + "\"></i>");
+  
+  // Return.
   return adjusted;
 }
 
 Discourse.Dialect.postProcessText(function (text) {
   text = [].concat(text);
+  // Cycle through all text.
   for (var i = 0; i < text.length; i++) {
     if (text[i].length > 0 && text[i][0] !== "<") {
+      // Send our text to the processor.
       text[i] = fontawesome(text[i]);
     }
   }
