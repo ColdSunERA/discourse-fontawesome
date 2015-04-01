ac93a8d311b5d7740b70d19b722f98c60af892f4
NomNuggetNom <nomnuggetnom@gmail.com>
2015-03-31 21:32:00 -0400

    Update fontawesome.js

--- assets/javascripts/fontawesome.js
+++ assets/javascripts/fontawesome.js
@@ -1,37 +1,8 @@
-//Use: [fa:iconname] ex [fa:circle]
-
-function fontawesome(text) {
-    // The expression we're looking for.
-    var re = /\[fa:\s*([a-zA-Z0-9-\s]*)]/g;
-    // Check to see if our regex is found.
-    if (re.test(text)) {
-        // Find potential arguments.
-        var args = (text.replace(re, "$1")).split(" ");
-        // Check our arguments.
-        if (args.length > 0) {
-            // Start adjusting.
-            var adjusted = "<i class=\"fa ";
-            for (var i = 0; i < args.length; i++) {
-                // Cycle through arguments and add each one.
-                adjusted = adjusted + "fa-" + args[i] + " ";
-            }
-            // Close our HTML.
-            adjusted = (adjusted + "\"></i>").replace(" \"", "\"");
-        }
-        return adjusted;
-    } else {
-        return text;
-    }
-}
-
-Discourse.Dialect.postProcessText(function (text) {
-  text = [].concat(text);
-  // Cycle through all text.
-  for (var i = 0; i < text.length; i++) {
-    if (text[i].length > 0 && text[i][0] !== "<") {
-      // Send our text to the processor.
-      text[i] = fontawesome(text[i]);
-    }
+Discourse.Dialect.inlineRegexp({
+  start: '[fa:',
+  matcher: /^\[fa:([a-z\-]+)\]/
+  emitter: function(match) {
+    var icon = match[1];
+    return ['i', {class: ['fa', 'fa-' + icon]} ];
   }
-  return text;
 });
