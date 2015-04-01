ce2248634db0f8cff8b12067c0e65c0083da5a8d
NomNuggetNom <nomnuggetnom@gmail.com>
2015-03-31 22:30:53 -0400

    Remove debug

--- assets/javascripts/fontawesome.js
+++ assets/javascripts/fontawesome.js
@@ -4,7 +4,6 @@
     matcher: /^\[fa:([a-z\-]+)\]/,
     emitter: function(contents) {
       var icon = contents[1];
-      console.log(icon);
       return ['i', {class: ['fa fa-' + icon]} ];
     }
   });
