bc4f39f90311562c1b2e418b4cf972c6d42c3100
NomNuggetNom <nomnuggetnom@gmail.com>
2015-03-31 22:03:41 -0400

    Update fontawesome.js

--- assets/javascripts/fontawesome.js
+++ assets/javascripts/fontawesome.js
@@ -1,9 +1,9 @@
-Discourse.Dialect.inlineRegexp({
+(function() {
   start: '[fa:',
-  matcher: /^\[fa:([a-z\-]+)\]/
-  emitter: function(match) {
-    var icon = match[1];
+  matcher: /^\[fa:([a-z\-]+)\]/,
+  emitter: function(contents) {
+    var icon = contents[1];
     return ['i', {class: ['fa fa-' + icon]} ];
   }
   Discourse.Markdown.whiteListTag('i', 'class', /fa-*/);
-});
+})();
